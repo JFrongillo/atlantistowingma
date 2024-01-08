82d78f421714970ffc4b6e7b8d4cbece31fffb0c
Joseph Frongillo <j_frongillo@salemstate.edu>
2024-01-07 22:09:33 -0500

    header items being added

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,9 +22,11 @@ export default function Header() {
             <li onClick={toggleDropdown} class = {isDropdownOpen ? `active` : ''}>Services</li>
             {isDropdownOpen && (
               <ul class="dropdown-menu">
-                <li>Service 1</li>
-                <li>Service 2</li>
-                <li>Service 3</li>
+                <li>Towing</li>
+                <li>Storage</li>
+                <li>Tire Change</li>
+                <li>Mechanical Works</li>
+                <li>Collison Repair</li>
               </ul>
             )}
           </div>
