c6e36d2f647cd2283eb8130008b9d92a6e9b1506
JFrongillo <j_frongillo@salemstate.edu>
2024-01-11 14:45:06 -0500

    Mobile responsive Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,14 @@ import React,  {useState}from 'react';
 
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+  }
+
+  const toggleMobile = () => {
+    setIsMobileOpen(!isMobileOpen);
   }
 
   return (
@@ -13,6 +18,7 @@ export default function Header() {
       <div class="header-item">
         <div class="header-logo"/>
       </div>
+      
       <div class="header-item">
         <ul class = "nav-items">
           <li>Home</li>
@@ -33,6 +39,32 @@ export default function Header() {
       <div class="header-item">
         <a href="#">Contact Us</a>
       </div>
+      
+      <div class ="hamburger header-item" onClick = {toggleMobile}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {isMobileOpen && (
+        <div class="mobile-menu">
+          <ul class="mobile-menu-items">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Testimonials</li>
+            <div class="dropdown-container">
+              <li onClick={toggleDropdown}>Services</li>
+              {isDropdownOpen && (
+                <ul class="dropdown-menu">
+                  <li>Service 1</li>
+                  <li>Service 2</li>
+                  <li>Service 3</li>
+                </ul>
+              )}
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
