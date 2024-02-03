d04beff53614ad9b3e22bbc968022a383db1d0b3
JFrongillo <j_frongillo@salemstate.edu>
2024-02-03 13:30:12 -0500

    mc

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -24,31 +24,41 @@ export default function Main() {
                 <h1>We Offer Most Services:</h1>
                 <div class="services-cards">
                     <div class="services-card">
-                        <div class="services-card-icon towing"></div>
-                        <h2>Towing</h2>
-                        <p>With our reliable towing service and wide fleet of veichles, we can tow: cars, trucks, SUV's, minivans, and even trucks that transport water tanks.</p>
+                        <div class="services-card-icon towing"/>
+                        <div className="card-text">
+                            <h2>Towing</h2>
+                            <p>With our reliable towing service and wide fleet of veichles, we can tow: cars, trucks, SUV's, minivans, and even trucks that transport water tanks.</p>
+                        </div>
                     </div>
                     <div class="services-card">
-                        <div class="services-card-icon garage"></div>
-                        <h2>Storage</h2>
-                        <p>Benefit from our secure vehicle storage services, offering advanced security and full legal compliance for both short-term and long-term needs.</p>
+                        <div class="services-card-icon garage"/>
+                        <div className="card-text">
+                            <h2>Storage</h2>
+                            <p>Benefit from our secure vehicle storage services, offering advanced security and full legal compliance for both short-term and long-term needs.</p>
+                        </div>
                     </div>
                     <div class="services-card">
-                        <div class="services-card-icon battery"></div>
-                        <h2>Tire Changes and Jumpstarts</h2>
-                        <p>Our rapid roadside assistance service efficiently handles flat tires and battery jumps, ensuring you're back on the road swiftly!</p>
+                        <div class="services-card-icon battery"/>
+                        <div className="card-text">
+                            <h2>Tire Changes</h2>
+                            <p>Our rapid roadside assistance service efficiently handles flat tires and battery jumps, ensuring you're back on the road swiftly!</p>
+                        </div>
                     </div>
                     <div class="services-card">
-                        <div class="services-card-icon wrench"></div>
-                        <h2>Vehicle Repairs</h2>
-                        <p>Our in-house mechanic shop features skilled technicians for comprehensive repair and maintenance, ensuring your vehicle's optimal performance.</p>
+                        <div class="services-card-icon wrench"/>
+                        <div className="card-text">
+                            <h2>Vehicle Repairs</h2>
+                            <p>Our in-house mechanic shop features skilled technicians for comprehensive repair and maintenance, ensuring your vehicle's optimal performance.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="screenshots-container">
-
+            <div class="guarantee-container">
+                <div className="guarantee-text">
+                    <h1>Professional Roadside Assistance Services Is One Phone Call Away in Revere, MA</h1>
+                    <p>At Atlantis Towing, we are committed to providing our customers with the highest level of service. Our team of professionals will work with you to find the best solution for your needs. We are available 24/7 to assist you with all of your towing and roadside assistance needs. Contact us today for more information!</p>
+                </div>
             </div>
-            <hr />
         </div>
     );
 }
