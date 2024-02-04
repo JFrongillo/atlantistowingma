e3b30d336d7b5f6a0a134d918f4050194cfa5d59
Joseph Frongillo <j_frongillo@salemstate.edu>
2024-02-03 21:38:34 -0500

    Buttons added to the services

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -13,8 +13,8 @@ export default function Main() {
             </div>
             <div class="about-container">
                 <div class="about">
-                    <div className="about-image"/>
-                    <div className="about-text">
+                    <div class="about-image"/>
+                    <div class="about-text">
                         <h1>About Us</h1>
                         <p>Atlantis Towing is a family owned and operated business that has been providing towing services in the Massachusetts area for over 20 years. We are a fully licensed and insured company that is committed to providing our customers with the highest level of service. We offer a wide range of services including towing, roadside assistance, and more. Our team of professionals will work with you to find the best solution for your needs. Contact us today for more information!</p>
                     </div>
@@ -25,38 +25,45 @@ export default function Main() {
                 <div class="services-cards">
                     <div class="services-card">
                         <div class="services-card-icon towing"/>
-                        <div className="card-text">
+                        <div class="card-text">
                             <h2>Towing</h2>
                             <p>With our reliable towing service and wide fleet of veichles, we can tow: cars, trucks, SUV's, minivans, and even trucks that transport water tanks.</p>
+                            <a href = "#">Get a Quote!</a>
                         </div>
                     </div>
                     <div class="services-card">
                         <div class="services-card-icon garage"/>
-                        <div className="card-text">
+                        <div class="card-text">
                             <h2>Storage</h2>
                             <p>Benefit from our secure vehicle storage services, offering advanced security and full legal compliance for both short-term and long-term needs.</p>
+                            <a href = "#">Store with us today!</a>
                         </div>
                     </div>
                     <div class="services-card">
                         <div class="services-card-icon battery"/>
-                        <div className="card-text">
+                        <div class="card-text">
                             <h2>Tire Changes</h2>
-                            <p>Our rapid roadside assistance service efficiently handles flat tires and battery jumps, ensuring you're back on the road swiftly!</p>
+                            <p>Our rapid roadside assistance service efficiently handles flat tires, battery jumps, and fuel deliveries, ensuring you're back on the road swiftly!</p>
+                            <a href = "#">Call us Today!</a>
                         </div>
                     </div>
                     <div class="services-card">
                         <div class="services-card-icon wrench"/>
-                        <div className="card-text">
+                        <div class="card-text">
                             <h2>Vehicle Repairs</h2>
                             <p>Our in-house mechanic shop features skilled technicians for comprehensive repair and maintenance, ensuring your vehicle's optimal performance.</p>
+                            <a href = "#">Contact Us Today!</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="guarantee-container">
-                <div className="guarantee-text">
+                <div class="guarantee-text">
                     <h1>Professional Roadside Assistance Services Is One Phone Call Away in Revere, MA</h1>
                     <p>At Atlantis Towing, we are committed to providing our customers with the highest level of service. Our team of professionals will work with you to find the best solution for your needs. We are available 24/7 to assist you with all of your towing and roadside assistance needs. Contact us today for more information!</p>
+                </div>
+                <div class = "reviews-container">
+                    
                 </div>
             </div>
         </div>
