4783914738de0cdfde5db9e5d1242cc2f72a3d0b
JFrongillo <j_frongillo@salemstate.edu>
2024-02-06 00:09:51 -0500

    about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,7 +1,43 @@
+import "./About.css";
+
 export default function About() {
     return(
         <div>
-            <h1>About</h1>
+            <div class="about-hero-container">
+                <h1>About Us</h1>
+            </div>
+            <div class="about-content-container">
+                <div class="about-content">
+                    <h1>More Than Professional Towing Service Providers</h1>
+                    <p>
+                        Over time, focusing on quality work, Atlantis Towing has become a premier choice for many people around. 
+                        Located in Revere, MA, our company offers a professional <a href = "/">towing service</a> with recovery 
+                        solutions in the form of mechanical work and collision repairs, 
+                        as well as a variety of other roadside services. Working with us is ideal. 
+                        Want to find out why? Here, you can.
+                    </p>
+                    
+                    <strong>Why Choose Our Company</strong>
+                    <p>
+                        We are a team of experienced and dedicated professionals who are always ready to help you. 
+                        We are well-prepared to provide you with the timely and reliable services you need. 
+                        Our rates are reasonable, and we are available 24/7. 
+                        We are always ready to help you, and we are sure to meet your requirements. 
+                    </p>
+
+                    <strong>How We Are Different</strong>
+                    <p>
+                        It is our 20-plus years of experience, the wealth of knowledge we possess, the techniques we apply, and the teamwork we demonstrate that makes Atlantis Towing a reputable business. 
+                        We stay up-to-date with new and efficient ways of handling each towing service and recovery task our clients hire us for in a timely and hassle-free manner. 
+                        With guarantee your complete satisfaction.
+                    </p>
+                    <p>
+                        Contact our roadside assistance company in Revere, MA if you are interested in the professional towing, mechanical works, collision service, and other tasks we can provide for you. 
+                        You can learn more about our business, request an estimate on our labor, and make an appointment by dialing (781) 485-1800. 
+                        We look forward to hearing from you. Give us a call today! 
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
